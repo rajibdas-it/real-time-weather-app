ee9af847c236b266493eeaa3a8b01e8bdebfa0c2
rajibdas-it <rajibrad@gmail.com>
2024-09-12 01:53:08 +0600

    fetching and view data on ui

--- src/Components/Weather/WeatherHeadline.jsx
+++ src/Components/Weather/WeatherHeadline.jsx
@@ -1,18 +1,22 @@
 import pin from "../../assets/pin.svg";
 import cloud from "../../assets/cloud.svg";
+import { useContext } from "react";
+import { WeatherContext } from "../../context";
 
 const WeatherHeadline = () => {
+  const { loading, weatherData, error } = useContext(WeatherContext);
+  console.log(loading, weatherData, error);
   return (
     <div>
       <div className="max-md:flex items-center justify-between md:-mt-10">
         <img src={cloud} alt="cloud" />
         <div className="max-md:flex items-center max-md:space-x-4">
           <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
-            16°
+            {Math.round(weatherData.temp)}°
           </h1>
           <div className="flex items-center space-x-4 md:mb-4">
             <img src={pin} />
-            <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
+            <h2 className="text-2xl lg:text-[50px]">{weatherData.name}</h2>
           </div>
         </div>
       </div>
