f8c90334d8d645ca327a1e80b2d33d865d689939
rajib <rajibrad@gmail.com>
2024-09-11 17:27:12 +0600

    customize tailwind config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./Components/Header/Header";
+import WeatherBoard from "./Components/Weather/WeatherBoard";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
     </>
   );
 };
