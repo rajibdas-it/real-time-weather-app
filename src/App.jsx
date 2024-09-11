b581e6cde6f5af5ee9a07d54a8c541a9bd69b97e
rajibdas-it <rajibrad@gmail.com>
2024-09-12 01:43:03 +0600

    create context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Header from "./Components/Header/Header";
 import WeatherBoard from "./Components/Weather/WeatherBoard";
+import WeatherProvider from "./provider/WeatherProvider";
 
 const App = () => {
   return (
-    <div className="grid place-items-center h-screen">
-      <Header />
-      <main>
-        <section>
-          <WeatherBoard />
-        </section>
-      </main>
-    </div>
+    <WeatherProvider>
+      <div className="grid place-items-center h-screen">
+        <Header />
+        <main>
+          <section>
+            <WeatherBoard />
+          </section>
+        </main>
+      </div>
+    </WeatherProvider>
   );
 };
 
---
+++ src/provider/WeatherProvider.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import { WeatherContext } from "../context";
+import useWeather from "../hooks/useWeather";
+
+const WeatherProvider = ({ children }) => {
+  const { weatherData, loading, error } = useWeather();
+  return (
+    <WeatherContext.Provider value={{ weatherData, loading, error }}>
+      {children}
+    </WeatherContext.Provider>
+  );
+};
+
+export default WeatherProvider;
