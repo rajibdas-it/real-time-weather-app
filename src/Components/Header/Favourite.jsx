6fec0ab854efafd164b77172626171335c77a1f3
rajib <rajibrad@gmail.com>
2024-09-12 16:55:11 +0600

    fav list modal done

--- src/Components/Header/Favourite.jsx
+++ src/Components/Header/Favourite.jsx
@@ -3,12 +3,9 @@ import favImg from "../../assets/heart.svg";
 
 const Favourite = ({ onShow }) => {
   return (
-    <div
-      onClick={onShow}
-      className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all"
-    >
+    <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
       <img src={favImg} alt="" />
-      <span>Favourite Locations</span>
+      <span onClick={onShow}>Favourite Locations</span>
     </div>
   );
 };
--- src/Components/Header/FavouriteListModal.jsx
+++ src/Components/Header/FavouriteListModal.jsx
@@ -1,11 +1,22 @@
+import { useContext } from "react";
+import { FavouriteContext } from "../../context";
+
 const FavouriteListModal = () => {
+  const { favs } = useContext(FavouriteContext);
+
   return (
     <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg ">
       <h3 className="text-lg font-bold px-4">Favourite Locations</h3>
       <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
-        <li className="hover:bg-gray-200">Dhaka</li>
-        <li className="hover:bg-gray-200">Rangpur</li>
-        <li className="hover:bg-gray-200">Europe</li>
+        {favs.length > 0 ? (
+          favs.map((fav, index) => (
+            <li key={index} className="hover:bg-gray-200">
+              {fav.location}
+            </li>
+          ))
+        ) : (
+          <p>Nothing is added to Favourites</p>
+        )}
       </ul>
     </div>
   );
